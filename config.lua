fb45a60b02e9ca855587d619fd3273b364147e8c
facoe <55077903+facoe@users.noreply.github.com>
2025-09-13 09:07:31 -0600

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,159 @@
+Config = {}
+
+-- VU GABZ
+
+Config.Locations18 = { -- doble Lounge
+	{ x = 113.36, y = -1303.80, z = 29.40, heading = 280.00 }	
+}
+
+Config.Locations19 = { -- mesa 1
+	{ x = 112.19, y = -1283.25, z = 28.88, heading = 130.00 } 
+}  
+
+Config.Locations20 = { -- mesa 2
+	{ x = 115.94, y = -1286.72, z = 28.88, heading = 215.0 } 		
+}
+
+Config.Locations21 = { -- dj
+	{ x = 121.40, y = -1281.49, z = 29.48, heading = 115.0 }		
+} 
+
+Config.Locations22 = { -- poldancer derecha
+	{ x = 102.29, y = -1289.85, z = 29.25, heading = 300.00 }		
+} 
+ 
+Config.Locations23 = { -- poldancer izquierda
+	{ x = 104.82, y = -1294.20, z = 29.25, heading = 300.00 }		
+}
+
+Config.Locations24 = { -- sola priv
+	{ x = 120.53, y = -1297.53, z = 29.27, heading = 30.00 }		
+}
+
+Config.Locations25 = { -- poldancer centro
+	{ x = 108.80, y = -1289.00, z = 29.25, heading = 300.00 }
+} 
+
+Config.Locations26 = { -- poldancer
+	-- { x = 104.82, y = -1294.20, z = 29.25, heading = 245.00 }
+}  
+
+-- BAHAMAS GABZ
+
+Config.Locations27 = { -- doble dj
+	{ x = -1378.99, y = -610.17, z = 30.35, heading = 120.00 }	
+} 
+
+Config.Locations28 = { -- poldancer boss
+	{ x = -1369.98, y = -621.06, z = 30.5, heading = 170.00 } 
+}  
+
+ Config.Locations29 = { -- pool 1
+	{ x = -1382.55, y = -604.04, z = 30.92, heading = 215.00 } 		
+} 
+
+Config.Locations30 = { -- pool 2
+	{ x = -1375.81, y = -614.38, z = 30.92, heading = 90.0 }		
+}  
+
+Config.Locations31 = { -- poldancer 1
+	{ x = -1387.83, y = -621.13, z = 30.7, heading = 120.00 }		
+} 
+
+Config.Locations32 = { -- poldancer 2
+	{ x = -1390.81, y = -616.56, z = 30.7, heading = 300.00 }		
+}
+
+Config.Locations33 = { -- poldancer 3
+	{x = -1393.78, y = -611.99, z = 30.7, heading = 120.00  }		
+} 
+
+Config.Locations34 = { -- poldancer derecha 1
+	{ x = -1409.15, y = -606.55, z = 30.5, heading = 350.00 }
+} 
+
+Config.Locations35 = { -- poldancer izquierda 3
+	{ x = -1391.30, y = -632.02, z = 31.3, heading = 350.00 }
+}  
+
+---------------------
+
+-- VU GABZ FACOE
+
+--[[ 
+Config.Locations18 = { -- doble premium
+	{ x = 137.85, y = -1289.82, z = 28.70, heading = 340.0 }	
+}
+
+Config.Locations19 = { -- mesa suelo
+	{ x = 111.35, y = -1285.10, z = 29.05, heading = 350.00 } 
+}  
+
+Config.Locations20 = { -- mesa 2
+	{ x = 119.41, y = -1287.98, z = 29.05, heading = 120.00 }		
+}
+
+Config.Locations21 = { -- office 
+	{ x = 109.65, y = -1312.04, z = 29.64, heading = 215.00 }		
+} 
+
+Config.Locations22 = { -- poldancer derecha
+	{ x = 102.29, y = -1289.85, z = 29.25, heading = 300.00 }		
+} 
+
+Config.Locations23 = { -- poldancer izquierda
+	{ x = 104.82, y = -1294.20, z = 29.25, heading = 300.00 }	
+}
+
+Config.Locations24 = { -- priv
+	{ x = 120.45, y = -1297.50, z = 29.27, heading = 30.00 }		
+}
+
+Config.Locations25 = { -- poldancer premium
+	{ x = 133.43, y = -1279.35, z = 29.29, heading = 210.00 }
+}
+
+Config.Locations26 = { -- poldancer lounge
+	{ x = 113.36, y = -1302.8, z = 29.28, heading = 300.00 }
+} 
+ ]]
+
+---------
+
+-- VANILLA UNICORN NO MLO
+
+--[[ 
+Config.Locations18 = { -- doble priv
+	{ x = 120.20, y = -1297.42, z = 28.75, heading = 30.00 }	
+} 
+
+Config.Locations19 = { -- suelo centro
+	{ x = 107.65, y = -1290.00, z = 28.86, heading = 300.00 } 
+}  
+
+ Config.Locations20 = { -- mesa 1
+	{ x = 106.51, y = -1284.32, z = 28.26, heading = 35.00 } 		
+} 
+
+Config.Locations21 = { -- mesa 2
+	{ x = 117.93, y = -1283.41, z = 28.28, heading = 315.0 }		
+}  
+
+Config.Locations22 = { -- poldancer derecha
+	{ x = 102.28, y = -1290.60, z = 29.25, heading = 300.00 }		
+} 
+ 
+Config.Locations23 = { -- poldancer izquierda
+	{ x = 104.22, y = -1294.00, z = 29.25, heading = 300.00 }		
+}
+
+Config.Locations24 = { -- sola priv
+	{x = 123.7, y = -1295.07, z = 29.27, heading = 35.00  }		
+} 
+
+Config.Locations25 = { -- poldancer centro
+	{ x = 112.62, y = -1286.74, z = 28.46, heading = 300.00 }
+}  
+]]
+
+
